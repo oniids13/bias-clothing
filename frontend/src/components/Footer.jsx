0530abd70a818d1e01e85593cbd4ebb37b5ab13d
oniids <jdino.abaya@gmail.com>
2025-07-02 15:16:37 +0800

    change x to tiktok icon, add a tag

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,21 @@
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import XIcon from "@mui/icons-material/X";
+import { FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="bg-black min-h-20 p-5 text-white">
       <div className="flex flex-col items-center gap-3 h-full md:grid md:grid-cols-3 md:items-center md:gap-0">
         <div className="socials flex gap-4">
-          <FacebookIcon />
-          <InstagramIcon />
-          <XIcon />
+          <a href="https://www.facebook.com/" target="_blank">
+            <FacebookIcon />
+          </a>
+          <a href="https://www.instagram.com/" target="_blank">
+            <InstagramIcon />
+          </a>
+          <a href="https://www.tiktok.com/" target="_blank">
+            <FaTiktok className="w-6 h-6" />
+          </a>
         </div>
         <p className="text-center text-sm md:text-base order-2 md:order-none ">
           &copy; {new Date().getFullYear()} Bias Clothing. All rights reserved.
