ba061e652d3b9454c55b90b9c5d1e072408bd9ce
oniids <jdino.abaya@gmail.com>
2025-07-14 15:44:45 +0800

    filter active and inactive products in backend

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -7,7 +7,10 @@ import {
 const getAllProductsController = async (req, res) => {
   try {
     const products = await getAllProducts();
-    res.status(200).json(products);
+    const activeProducts = products.filter((product) => product.isActive);
+    const inactiveProducts = products.filter((product) => !product.isActive);
+    const allProducts = [...activeProducts, ...inactiveProducts];
+    res.status(200).json(allProducts);
   } catch (error) {
     res.status(500).json({ message: "Error fetching products" });
   }
